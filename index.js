4f9336e63a4930975012090c7eebd0a5221d7a54
Aggelos <87412823+AggelosLua@users.noreply.github.com>
2021-10-17 14:10:18 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+require('http').createServer((req, res) => res.end('Hello!')).listen(80);
+//Hosting
+//Message Functions:
+//msg.startsWith
+//EndsWith()
+//match(regexp)
+//replace(regexp/substr, replacetext)
+//search(regexp)
+//slice(start, [end])
+//split(delimiter, [limit])
+//substr(start, [length])
+//substring(from, [to])
+//toLowerCase()
+//toUpperCase()
+//includes()
+//endsWith()
+//repeat()
+//valueOf()
+//trim()
+
+//Operators:
+// || (OR operator)
+// && (And operator)
+// +	(Add up a value (numeric))
+// ++	(Increment operator. Increases a vlue by 1)
+// /	Divide left operand by right operand.
+// -- (Decrement a value by 1)
+// *	(Multiply two numeric values)
+// ==	(Compare equality of two values with ignoring type)
+// ===	(Compares equality of two values with type)
+// 	! (NOT operator)
